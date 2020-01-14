8120bb59d10f2c1ebe980de172f92989bb670355
siwarjelassi <56412397+siwarjelassi@users.noreply.github.com>
2020-01-14 21:09:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(".image1").mouseover(function () {
+    $(".divbutton1").show()
+})
+
+$(".image1").mouseleave(function () {
+    $(".divbutton1").hide()
+})
+
+
+$(".image2").mouseover(function () {
+    $(".divbutton1").show()
+})
+
+$(".image2").mouseleave(function () {
+    $(".divbutton1").hide()
+})
+
+//---------------------------//
